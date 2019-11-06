d2396c6836c3297e8a592307a5e98127fe08050d
saisua <ausias.prieto@gmail.com>
2019-11-06 10:23:25 +0100

    Added new untested features

--- Whatsapp_api.js
+++ Whatsapp_api.js
@@ -1,5 +1,5 @@
 ﻿	// Send 
-function send_message(text, times=1){
+function send_message(text, times=1, selfdestruct=false){
 	if(Array.isArray(text)){
 		for(mes=0;mes < text.length; mes++){send_message(text[mes], times);}
 		return;
@@ -9,7 +9,9 @@ function send_message(text, times=1){
         input.innerHTML = text;
         input.dispatchEvent(new Event('input', {bubbles: true}));
 		var button = get_button();
-        button.click();}
+        button.click();
+		last = last_message();
+		if(selfdestruct){setInterval(function(){if(seen(last)){delete_message(last);}},2)}}
 	,1000);}
 }
 
@@ -31,10 +33,17 @@ function create_date(year=new Date().getFullYear(), month=new Date().getMonth(),
 
 }
 
+function do_in(to_do, args, miliseconds=5000){
+	setTimeout(function(){to_do(...args)}, miliseconds)		
+}
+
+function do_at(to_do, args, timedate=create_date()){
+	setTimeout(function(){to_do(...args)},timedate - new Date());
+}
+
 // Send at specific time 
 function send_at(text, timedate=create_date(), send_times=1){
-	var miliseconds = timedate - new Date();
-	setTimeout(function(){send(text, send_times);},miliseconds);
+	do_at(send,[text, send_times], timedate);
 }
 
 // Mention
@@ -49,56 +58,14 @@ function number_to_mention(input, num, nom="Easteregg"){
 	input.appendChild(node3);
 }
 
-// Erase
-// Borrar n missatges [Inf] en base al innertext
-function remove(message, for_everyone=true)
-{
-	/*
-	var first, node, nodeaux;
-	first = document.createElement("div"); first.setAttribute("class","_3EQsG _15CAo"); first.setAttribute("style","opacity: 1;");c
-	nodeaux = document.createElement("span"); nodeaux.setAttribute("class","_2-qoA"); 
-		nodeaux.setAttribute("data-js-context-icon","true"); nodeaux.setAttribute("style","transform: translateX(0%);");
-	first.appendChild(nodeaux);
-	node = document.createElement("svg"); node.setAttribute("id","Layer_1"); node.setAttribute("xmlns","http://www.w3.org/2000/svg"); 
-		node.setAttribute("viewBox","0 0 18 18"); node.setAttribute("width","18"); node.setAttribute("height","18");
-	nodeaux.appendChild(node);
-	nodeaux = document.createElement("path"); nodeaux.setAttribute("fill-opacity",".45"); 
-		nodeaux.setAttribute("d","M3.3 4.6L9 10.3l5.7-5.7 1.6 1.6L9 13.4 1.7 6.2l1.6-1.6z");
-	message.appendChild(first);
-	return first;
-	*/
-}
-
-/*
-function private_remove(element){
-	var node = document.createElement("div"); node.setAttribute("class", "_2eK7W _23_1v"), node.setAttribute("role","button");
-	node.onclick = function() {
-  var e = i.props,
-    t = e.chat, //								 M15.9 14.3H15l-.3-.3c1-1.1 1.6-2.7 1.6-4.3 0-3.7-3-6.7-6.7-6.7S3 6 3 9.7s3 6.7 6.7 6.7c1.6 0 3.2-.6 4.3-1.6l.3.3v.8l5.1 5.1 1.5-1.5-5-5.2zm-6.2 0c-2.6 0-4.6-2.1-4.6-4.6s2.1-4.6 4.6-4.6 4.6 2.1 4.6 4.6-2 4.6-4.6 4.6z
-    a = e.msgList, // d? <path fill="#92A58C" d="M10.91 3.316l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.879a.32.32 0 0 1-.484.033L1.891 7.769a.366.366 0 0 0-.515.006l-.423.433a.364.364 0 0 0 .006.514l3.258 3.185c.143.14.361.125.484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z"></path>
-    n = e.toastPosition, (0,0)
-    o = e.onCancel,
-    r = e.onDelete;
-  w.default.sendDeleteMsgs(t, a, i.state.clearMedia, null, n), r(), o && o()
-	};
-	node.click = function(){};
-}
-*/
-
-/*<div class="_3EQsG _15CAo" style="opacity: 1;">
-<div data-js-context-icon="true" class="_2-qoA" style="transform: translateX(0%);">
-<span data-icon="down-context" class="">
-<svg id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18" width="18" height="18">
-<path fill-opacity=".45" d="M3.3 4.6L9 10.3l5.7-5.7 1.6 1.6L9 13.4 1.7 6.2l1.6-1.6z">
-</path></svg></span></div></div>
-*/
 
 // Global definition of messageSelector
 window.messageSelector = {"text":"_12pGw",
 							"emoji":"_12pGw EopGb",
 							"image":"_3mdDl'] img[class='_18vxA",
 							"audio":"uqRgA _38Akx'] div[class='TeXXU'] audio"}; 
-
+window.was_connected = false;
+							
 // get last chat message
 function last_message(num=1){	
 	if(num<1){return;}
@@ -165,15 +132,22 @@ function index(message, max_find=10){
 	return null;
 }
 
+function seen(){return true;}
+
 // Check and answer
-function check_if_answer(reply_dict={}, return_on_answer=true, miliseconds=1000, do_not_found=[function(){},[]]){
+function check_if_answer(reply_dict={}, return_on_answer=true, miliseconds=1000, do_not_found=[function(){},[]],
+						show_time=true, check_connected=true){
 	var lmess = last_message();
 	var aux = null;
 	window.check = setInterval(function(){
 		lmess = last_message();
+		check_if_connected(show_time, check_connected);
 		if(lmess != aux){
 			var aux_index = index(aux);
 			if(aux_index === null){aux=lmess; return;}
+			
+			if(show_time){console.log(date_str());}
+			
 			for(counter = aux_index; counter > 0; counter--){
 				console.log("message detected = " + message_to_string(last_message(counter)));
 				answer(lmess, reply_dict, do_not_found, return_on_answer);
@@ -184,6 +158,17 @@ function check_if_answer(reply_dict={}, return_on_answer=true, miliseconds=1000,
 	document.addEventListener("keydown", function(event){if(event.which==27){clearInterval(check);console.log("check stopped");};}); 
 	console.log("Started check in background. press Esc (in site not console) to stop")
 	console.log("or run 'clearInterval(checking);'")
+}
+
+function check_if_connected(show_time=true, print=true){
+	if(was_connected != is_connected()){
+		if(print){
+			if(was_connected){console.log("Disconnected");}
+			else{console.log("Connected");}
+			if(show_time){console.log(date_str()); show_time=false;}
+		}
+		was_connected = !was_connected;
+	}
 }
 
 // save to local file or new tab
@@ -199,6 +184,44 @@ function store(text, name, local=false, type="text/plain") {
   else{window.open(a.href,'_blank');}
 }
 
+function is_connected(){
+	var connection = document.querySelector("div[class='_3V5x5'] div[class='_3Q3ui i1XSV']");
+	if(connection){
+		if(connection.innerText.toLowerCase().includes("online") || 
+				connection.innerText.toLowerCase().includes("typing") || 
+				connection.innerText.toLowerCase().includes("recording audio")){return true;}
+	}
+	return false;
+}
 
+function date_str(date=new Date()){return date.getDate()+'/'+date.getMonth()+" @ "+
+											date.getHours()+':'+date.getMinutes()+':'+date.getSeconds();}
+		
+function sleep(ms){return new Promise(resolve => setTimeout(resolve, ms));}
+
+async function delete_message(messages, only_for_me=false){
+	if(Array.isArray(messages)){
+		for(message=0;message<messages.length;message++){delete_message(message);}
+		return;
+	}
+	var canvas = document.querySelector("div[class='_1_keJ']");
+	var event = canvas.ownerDocument.createEvent('MouseEvents');
+	
+	event.initMouseEvent('contextmenu', true, true, canvas.ownerDocument.defaultView,1,0,0,0,0,false,
+						false,false,false,2,null);
+						
+	!canvas.dispatchEvent(event);
+	
+	document.querySelector("li[class='_3cfBY _2yhpw _3BqnP'] div[title='Select messages']").click();
+	await sleep(1);							
+	var selectables = document.querySelectorAll("div[class='qTFAl']");
+	selectables[selectables.length-index(messages)].click();
+	document.querySelector("button[class='_1wRbe'][title='Delete message']").click();
+	var for_everyone = document.evaluate("//div[text()='Delete for everyone']",document,null,XPathResult.ANY_TYPE, null).iterateNext();
+	if(for_everyone && !only_for_me){
+		for_everyone.click();
+		document.querySelector("div[class='_2eK7W _3PQ7V']").click();
+	}else{document.evaluate("//div[text()='Delete for me']",document,null,XPathResult.ANY_TYPE, null).iterateNext().click();}
+}
 /*{"🤠":[send_message, ["Yiiiiihaaa!"]],"❤":[send_message, ["Coret"]],"♥":[send_message, [	"Coret"]]}
 */
